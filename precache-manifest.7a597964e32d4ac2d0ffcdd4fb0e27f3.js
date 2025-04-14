3f48f30924dfc29a8b0254518b5623827c279cfc
sjsheeja24 <sjsheeja24@gmail.com>
2025-04-14 18:00:43 +0530

    Updates

---
+++ precache-manifest.7a597964e32d4ac2d0ffcdd4fb0e27f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76e74ba0dc0c6c754d9be03a150ad40",
+    "url": "/ecommerce-site/index.html"
+  },
+  {
+    "revision": "1f00d41feb23968722d2",
+    "url": "/ecommerce-site/static/css/main.5f2f1d25.chunk.css"
+  },
+  {
+    "revision": "6553bf3739e9a48a0b38",
+    "url": "/ecommerce-site/static/js/2.907c97b7.chunk.js"
+  },
+  {
+    "revision": "359dbed9f99e788752d8fda758c83bcd",
+    "url": "/ecommerce-site/static/js/2.907c97b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5910bc3017dd51a1a08",
+    "url": "/ecommerce-site/static/js/3.fbbfe974.chunk.js"
+  },
+  {
+    "revision": "1f00d41feb23968722d2",
+    "url": "/ecommerce-site/static/js/main.a88247b8.chunk.js"
+  },
+  {
+    "revision": "11d86621889f36b4e503",
+    "url": "/ecommerce-site/static/js/runtime-main.1b966655.js"
+  }
+]);
